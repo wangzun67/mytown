688cf99f4918ee4acfc02f43b593fabd36bb88d5
wangzun <wangzun67@gmail.com>
2021-04-11 16:03:56 +0800

    fix bug

--- mytown/settings.py
+++ mytown/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "@^$ovif-+_zyf3nmin%57)hai)^lr$g)a_4=)zlrn+u$k3$f6&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
